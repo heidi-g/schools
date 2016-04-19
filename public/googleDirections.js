e273e2c8b83d0635dc2af278e9962dec0087cd33
Heidi <heidi@apartmentlist.com>
2016-04-19 18:32:20 +1200

    we have a map

--- public/googleDirections.js
+++ public/googleDirections.js
@@ -1,73 +1,29 @@
 
-
-// current location
-var map;
-var infoWindow;
-var service;
-var currentLat;
-var currentLong;
-
-
-var clickForLocation = document.getElementById("geoLocation")
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    // center: {lat: currentLat, lng: currentLong},
-    zoom: 15,
-    styles: [{
-      stylers: [{ visibility: 'complex' }]
-    }, {
-      elementType: 'labels',
-      stylers: [{ visibility: 'on' }]
-    }]
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var directionsService = new google.maps.DirectionsService;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: {lat: 37.77, lng: -122.447}
   });
+  directionsDisplay.setMap(map);
 
-if (navigator.geolocation) { /// start of geolocator
-          navigator.geolocation.getCurrentPosition(function(position) {
-            currentLat = position.coords.latitude
-            currentLong = position.coords.longitude
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            map.setCenter(pos);
-                  var marker = new google.maps.Marker({
-                   position:pos,
-                   map: map
-
-                  });
-            console.log(pos, "current positon")
-            console.log(currentLat, "current Lat")
-            console.log(currentLong, "current Long")
-          });
-        }  //end of
-
-    // The idle event is a debounced event, so we can query & listen without
-  // throwing too many requests at the server.
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
 }
 
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  var selectedMode = "DRIVING";
+  directionsService.route({
+    origin: {lat: 37.77, lng: -122.447},  // start
+    destination: {lat: 37.768, lng: -122.511},  // school
 
-// var directionsDisplay;
-// var directionsService = new google.maps.DirectionsService();
+    travelMode: google.maps.TravelMode[selectedMode]
+  }, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
 
-// function initialize() {
-//   directionsDisplay = new google.maps.DirectionsRenderer();
-//   // map = new google.maps.Map(document.getElementById("map"), mapOptions);
-//   // directionsDisplay.setMap(map);
-//   // directionsDisplay.setPanel(document.getElementById("directionsPanel"));
-// }
-
-// function calcRoute() {
-//   var start = "15 Allen St, Te Aro, Wellington 6011";
-//   var end = pos; //defined above
-//   var request = {
-//     origin:start,
-//     destination:end,
-//     travelMode: google.maps.TravelMode.DRIVING
-//   };
-//   directionsService.route(request, function(response, status) {
-//     if (status == google.maps.DirectionsStatus.OK) {
-//       directionsDisplay.setDirections(response);
-//     }
-//   });
-// }
