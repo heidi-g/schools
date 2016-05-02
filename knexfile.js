40eff1ecdf04c71e63aeeaabc09c33c01700e268
routes <email": null>
2016-05-03 08:33:28 +1200

    deploy to heroku

--- knexfile.js
+++ knexfile.js
@@ -5,33 +5,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      filename: 'dev.sqlite3'
     }
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL
     pool: {
       min: 2,
       max: 10
